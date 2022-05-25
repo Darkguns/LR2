d273fc5f3e2453cf75a94f2b39fb0bee5d24c811
Дмитрий Родионов АДБ-19-06 <rds26@mail.ru>
2022-05-26 02:44:15 +0300

    LR2
    LR2

---
+++ Удалить.bat
@@ -0,0 +1,20 @@
+echo off
+md Rodionov
+cd Rodionov
+md Dmitry
+md Sergeevich
+pause
+echo > 17121998.txt
+cd Sergeevich
+echo > computerM400009493.txt
+pause
+cd ..
+cd ..
+del Rodionov /S /Q /F
+pause
+cd Rodionov
+rd Sergeevich
+rd Dmitry
+cd ..
+rd Rodionov
+pause
---
+++ Фамилия.bat
@@ -0,0 +1,10 @@
+echo off
+md Rodionov
+cd Rodionov
+md Dmitry
+echo >17121998.txt
+cd Dmitry
+md Sergeevich
+cd  Sergeevich
+echo > computerM400009493.txt
+pause
